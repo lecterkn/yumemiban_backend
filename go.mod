0fb93c3277b5359d8094db8b52fbfb78977a7482
lecterkn <lecterkn@proton.me>
2025-03-06 22:33:13 +0900

    wireの定義ファイルを作成 #1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lecterkn/yumemiban_backend
 
 go 1.24.0
+
+require github.com/google/wire v0.6.0 // indirect
--- internal/app/di/wire.go
+++ internal/app/di/wire.go
@@ -1,1 +1,38 @@
+//go:build wireinject
+// +build wireinject
+
 package di
+
+import "github.com/google/wire"
+
+var databaseSet = wire.NewSet(
+// データベースのコネクタ
+)
+
+var repositorySet = wire.NewSet(
+// リポジトリの実装
+)
+
+var usecaseSet = wire.NewSet(
+// ユースケース
+)
+
+var handlerSet = wire.NewSet(
+// ハンドラ
+)
+
+// 生成されるハンドラ
+type HandlerSet struct {
+}
+
+// ハンドラセットを取得する
+func InitializeControllerSet() *HandlerSet {
+	wire.Build(
+		databaseSet,
+		repositorySet,
+		usecaseSet,
+		handlerSet,
+		wire.Struct(new(HandlerSet), "*"),
+	)
+	return nil
+}
